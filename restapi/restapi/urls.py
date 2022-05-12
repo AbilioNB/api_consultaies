ba243cb0caf7001b4cc055d6d6df20a353d780a1
abilionb <abilionbarros@gmail.com>
2022-05-11 22:24:35 -0300

    Add Swagger

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -15,8 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API-ConsultaIES",
+      default_version='v1',
+      description="API para a disponibilização de dados sobre a educação superior brasileira.",
+      terms_of_service="#",
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
+
+
+
+
